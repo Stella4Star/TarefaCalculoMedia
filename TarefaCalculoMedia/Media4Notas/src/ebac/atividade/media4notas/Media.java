2bd9e073c555a36030fb48fde50b6fa9bee15c86
Stella <samspchan@gmail.com>
2024-03-15 22:35:13 -0300

    Commit do exercício de calculo de média

---
+++ TarefaCalculoMedia/Media4Notas/src/ebac/atividade/media4notas/Media.java
@@ -0,0 +1,20 @@
+package ebac.atividade.media4notas;
+
+public class Media {
+	
+	public static void main(String args[]) {
+		int nota1;
+		int nota2;
+		int nota3;
+		int nota4;
+		double media;
+	
+		nota1 = 7;
+		nota2 = 8;
+		nota3 = 4;
+		nota4 = 10;
+		media = nota1 + nota2 + nota3 + nota4 / 4;
+		
+		System.out.println("A média do aluno é de: " + media);
+	}
+}
